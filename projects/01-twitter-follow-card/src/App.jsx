020ac679cab4be14d30faffe63e3a1d619b6b7f6
Albazc <albazcano@gmail.com>
2023-01-18 17:18:12 +0100

    modify app

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,30 +1,49 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
-export function App () {
+const users = [
+    {
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true,
+    },
+    {
+      userName: 'miriamgonp',
+      name: 'Miriam González',
+      isFollowing: true
+    },
+    {
+      userName: 'MoureDev',
+      name: 'Brais Moure',
+      isFollowing: false
+    },
+    {
+      userName: 'dawntraoz',
+      name: 'Alba Silvente',
+      isFollowing: false
+    },
+    {
+        userName: 'baumannzone',
+        name: 'Jorge Baumann',
+        isFollowing: true
+      }
+  ]
 
+export function App () {
     return (
         <section className='App'>
-        <TwitterFollowCard userName="midudev">
-         Miguel Ángel Durán
-        </TwitterFollowCard> 
-       
-        <TwitterFollowCard  userName="miriamgonp">
-        Miriam González
-        </TwitterFollowCard> 
-
-        <TwitterFollowCard userName="MoureDev"> 
-        Brais Moure
-        </TwitterFollowCard> 
-
-        <TwitterFollowCard userName="dawntraoz">
-        Alba Silvente
-        </TwitterFollowCard> 
-       
-        <TwitterFollowCard userName="baumannzone">
-        Jorge Baumann
-        </TwitterFollowCard> 
-         
+            {
+                users.map(({userName, name, isFollowing}) =>(
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                            </TwitterFollowCard>
+                    ))
+            }
+             
         </section>  
     )
 }
